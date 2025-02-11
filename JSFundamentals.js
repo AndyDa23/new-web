84bdfa345deabdedc4278fb57c87acc3bb6dca3c
Ceban Andrei <124030021+AndyDa23@users.noreply.github.com>
2025-02-11 11:12:00 +0200

    Add files via upload

---
+++ JSFundamentals.js
@@ -0,0 +1,26 @@
+/*let username;
+let password;
+
+function PassWDValid() {
+    username = prompt("Введите логин");
+    password = prompt("Введите пароль");
+
+    if (!username || !password) {
+        alert("Validation Error");
+
+    } else if (username === "AndyDa" && password === "123") {
+        alert(`Hello ${username}`);
+
+    } else if (username != "AndyDa") {
+        alert("Incorrect login");
+         if (password != "123") {
+        alert("Incorrect password");
+    }
+
+    }else {
+        alert("Login invalid");
+    }
+}
+
+
+PassWDValid();*/
